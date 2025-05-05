4fbec73d9a48d0e7378d432919fd28b855b69bf5
JomanaAmin <j.hussein2327@nu.edu.eg>
2025-05-05 18:11:10 +0300

    hhh
    hhhh

---
+++ MazeGenerator.py
@@ -0,0 +1,10 @@
+from Cell import Cell
+class MazeGenerator:
+    def __init__(self,size):
+        grid=[[Cell(x,y) for x in range(size)] for y in range(size)]
+        pass
+"""    def gridGenerator(self,size):
+        for y in range(size):
+            for x in range(size):
+                cell=Cell(x,y) """
+
